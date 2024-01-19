dc87d43e836587d18c163e43c807bc85f00780dc
Mike3osc <139455369+Mike3osc@users.noreply.github.com>
2024-01-19 08:23:50 +0100

    new push

--- Ej_3/index.js
+++ Ej_3/index.js
@@ -7,19 +7,34 @@ document.body.appendChild(countryList);
 
 for (const countrySetOne of countriesSetOne) {
   const countryLi = document.createElement("li");
-  console.log(countrySetOne);
 
   countryLi.append(countrySetOne);
-  console.log(countryLi)
   countryList.appendChild(countryLi)
 }
 
-
 //1.2 Elimina el elemento que tenga la clase .fn-remove-me.
+
+const deletedElments = document.querySelectorAll(".fn-remove-me");
+
+for (const deletedElement of deletedElments) {
+  deletedElement.remove()
+}
+
 
 //1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos 
 //en el div de html con el atributo data-function="printHere".
 const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+
+const div = document.querySelector("[data-function=printHere]");
+
+const carList = document.createElement("ul");
+div.appendChild(carList);
+
+for (const car of cars) {
+  const carLi = document.createElement("li");
+  carLi.append(car);
+  carList.appendChild(carLi);
+}
 
 //1.4 Crea dinamicamente en el html una serie de divs que contenga un elemento 
 //h4 para el titulo y otro elemento img para la imagen.
@@ -30,6 +45,21 @@ const countriesSetTwo = [
   { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4' },
   { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5' }
 ];
+
+for (const countrySetTwo of countriesSetTwo) {
+  const setOfDivs = document.createElement("div");
+  const titleH4 = document.createElement("h4");
+  const randomImg = document.createElement("img");
+
+  titleH4.textContent = countrySetTwo.title;
+  randomImg.src = countrySetTwo.imgUrl;
+
+  setOfDivs.appendChild(titleH4);
+  setOfDivs.appendChild(randomImg);
+
+  document.body.appendChild(setOfDivs);
+}
+
 
 //1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último
 //elemento de la serie de divs.
